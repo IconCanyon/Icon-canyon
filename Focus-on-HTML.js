daf96304352189c0259c2ef1dbcc2ecc9be4d18e
IconCanyon <iconcanyon@gmail.com>
2025-12-24 19:32:13 +0300

    Update Focus-on-HTML.js

--- Focus-on-HTML.js
+++ Focus-on-HTML.js
@@ -1,7 +1,8 @@
 (function enableFocus() {
-  document.querySelectorAll('div, span, a, label').forEach(el => {
+  document.querySelectorAll('span, a, label').forEach(el => {
     if (!el.hasAttribute('tabindex')) {
       el.setAttribute('tabindex', '0');
     }
   });
 })();
+
