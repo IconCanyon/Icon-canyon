fc27b4477e6fee62c5b68e0b4ae585bc703fd544
IconCanyon <iconcanyon@gmail.com>
2026-02-09 20:44:02 +0300

    Update share_3.0.0.js

--- share_3.0.0.js
+++ share_3.0.0.js
@@ -465,7 +465,7 @@
         <div class="textarea-container">
           <textarea class="share-textarea" name="textarea" placeholder="${placeholderText}">${initialText}</textarea>
           <div class="file-attachment-area">
-            <label id="fileAttachLabel" class="file-attach-btn Wave-center">
+            <label for="" id="fileAttachLabel" class="file-attach-btn Wave-center">
               <svg xmlns="http://www.w3.org/2000/svg" class="ionicon" viewBox="0 0 512 512">
                 <path d="M448 256c0-106-86-192-192-192S64 150 64 256s86 192 192 192 192-86 192-192z" fill="none" stroke="currentColor" stroke-miterlimit="10" stroke-width="32"/>
                 <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="32" d="M256 176v160M336 256H176"/>
@@ -746,3 +746,4 @@
 
   window.Share = { save, shareMultiple, loadAllData };
 })();
+
